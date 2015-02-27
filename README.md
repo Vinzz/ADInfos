5863bf31647a99f839001eda0133f890240b0fb7
Vinzz <vincent.tollu@gmail.com>
2015-02-27 18:45:17 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ADInfos
 Small powershell cmdlet to extract infos from an AD
+
+Credentials and AD adress are stored in the following environment variables:
+- LDAPLogin
+- LDAPPassword
+- LDAPAddress
---
+++ cmdlet/FindADInfosCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using ADInfosService;
+
+namespace cmdlet
+{
+
+    [Cmdlet(VerbsCommon.Find, "ADInfos")]
+    public class FindADInfosCommand : Cmdlet
+    {
+        private ADService service = new ADService();
+
+        // Declare the parameters for the cmdlet.
+        [Parameter(Mandatory = true)]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string name;
+
+       /// <summary>
+       /// Finds informations in the AD about a given name
+       /// <remarks>
+       /// Name
+       /// FirstName
+       /// Login
+       /// Service
+       /// </remarks>
+       /// </summary>
+        protected override void ProcessRecord()
+        {
+            foreach(UserInfo user in service.GetADInfos(name))
+            {
+                WriteObject(user);
+            }
+        }
+    }
+}
